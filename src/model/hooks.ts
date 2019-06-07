378c049a17efa09f5e62b50c9cf5c94a77bd0647
vedanta-krit.das <vedmalex@gmail.com>
2019-06-07 11:46:10 +0300

    fix: hooks ! maybe not working

--- src/model/hooks.ts
+++ src/model/hooks.ts
@@ -25,25 +25,25 @@ export let securityFields = {
 
 export let userPasswordStatus = {
   name: 'userPasswordStatus',
-  'entities.User.metadata.acl.read': 'public',
-  'entities.User.fields.password.metadata.acl.read': 'owner',
+  'entities.User.metadata.acl.read': ['public', 'system'],
+  'entities.User.fields.password.metadata.acl.read': ['owner', 'system'],
   'entities.User.fields.[enabled,isSystem,isAdmin].metadata.acl.read': {
     $assign: ['system'],
   },
-  'entities.User.fields.[id, userName, todos].metadata.acl.read': 'public',
+  'entities.User.fields.[id, userName, todos].metadata.acl.read': ['public', 'system'],
 };
 
 export let securityAcl = {
   name: 'security',
   'entities.^[User].fields.[createdBy,updateBy,createdAt, updatedAt].metadata.acl.read':
-    'system',
+    ['system'],
 };
 
 export let ownerFields = {
   name: 'security',
   [`entities.^[User].fields`]: [
     {
-      name: 'owner',
+      name: ['owner', 'system'],
       indexed: true,
     },
   ],
@@ -51,13 +51,13 @@ export let ownerFields = {
 
 export let ownerAcl = {
   name: 'security',
-  [`entities.^[User].fields.owner.metadata.acl.read`]: 'system',
+  [`entities.^[User].fields.owner.metadata.acl.read`]: ['system'],
 };
 
 export let defaultVisibility = {
   name: 'default visibility',
-  [`entities.*.metadata.acl.read`]: 'owner',
-  [`entities.*.fields.*.metadata.acl.read`]: 'owner',
+  [`entities.*.metadata.acl.read`]: ['owner', 'system'],
+  [`entities.*.fields.*.metadata.acl.read`]: ['owner', 'system'],
 };
 
 export const accessFixEntities = {
@@ -70,6 +70,7 @@ export const accessFixEntities = {
   'entities.*.fields.*.metadata.acl.update': [],
   'entities.*.metadata.acl.delete': [],
   'entities.*.fields.*.metadata.acl.delete': [],
+  'entities.*.fields.*.metadata.acl.shape': [],
 };
 
 export const accessFixMutations = {
@@ -79,13 +80,13 @@ export const accessFixMutations = {
 
 export let defaultMutationAccess = {
   name: 'Default Mutation access',
-  'mutations.*.metadata.acl.execute': 'owner',
-  'mutations.loginUser.metadata.acl.execute': 'public',
+  'mutations.*.metadata.acl.execute': ['owner', 'system'],
+  'mutations.loginUser.metadata.acl.execute': ['public', 'system'],
 };
 
 export let defaultIdVisibility = {
   name: 'default id field visibility',
-  'entities.*.fields.id.metadata.acl.read': 'public',
+  'entities.*.fields.id.metadata.acl.read': ['public', 'system'],
 };
 
 export let runtimeMutationAcl = {
